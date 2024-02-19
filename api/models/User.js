7efc951ba6a798676f738ac52a8d29252cdca1a7
Teory <berkay.koksall54@gmail.com>
2024-02-20 01:09:36 +0300

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Please provide a username'],
-        min: 4,
+        min: 6,
         unique: true
     },
     email: {
