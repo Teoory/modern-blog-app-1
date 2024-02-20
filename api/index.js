b0a8730278e49206404950fbd411546fe9513ac5
Teory <berkay.koksall54@gmail.com>
2024-02-20 23:08:45 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -190,7 +190,9 @@ app.post('/verify-email', async (req, res) => {
         if (!verification) {
           return res.status(404).json({ error: 'Geçersiz doğrulama kodu.' });
         }
-        if (User.tags === 'user') {
+
+        const UserCheck = await User.findOne({ username });
+        if (UserCheck.tags === 'user') {
         await User.findOneAndUpdate({ email: verification.email }, { isVerified: true, tags: ['writer'] });
         } else {
             await User.findOneAndUpdate({ email: verification.email }, { isVerified: true });
