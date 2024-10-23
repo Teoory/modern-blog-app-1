685448a9069a3f75ea2f0857498ae31e9fbbc1ab
Berkay Köksal <berkay.koksall54@gmail.com>
2024-10-23 22:40:03 +0300

    web and mobile add comment fix

--- api/index.js
+++ api/index.js
@@ -1120,13 +1120,11 @@ app.get('/tags/:tag', async (req, res) => {
 
 app.post('/post/:id/comment', uploadProfilePhoto.single('file'), async (req, res) => {
     const { id } = req.params;
-    try {
-        const { token } = req.cookies;
-    } 
-    catch {
-        const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(' ')[1];
-    }
+    const authHeader = req.headers['authorization'];
+
+    const token = req.cookies || (authHeader && authHeader.split(' ')[1]);
+    
+    // const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
         return res.status(401).json({ error: 'Token gereklidir' });
