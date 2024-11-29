123324be7945e2a8e240dd02a909de7bb04ef44d
Berkay Köksal <berkay.koksall54@gmail.com>
2024-11-29 20:42:59 +0300

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:3030';
-// export const API_BASE_URL = 'https://fiyasko-blog-api.vercel.app';
+// export const API_BASE_URL = 'http://localhost:3030';
+export const API_BASE_URL = 'https://fiyasko-blog-api.vercel.app';
