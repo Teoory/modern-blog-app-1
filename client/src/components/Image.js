8b793ffa512404cc3fcaf486b802e7a533f23109
Berkay Köksal <berkay.koksall54@gmail.com>
2025-01-06 20:21:58 +0300

    Update Image.js

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -1,5 +1,6 @@
 function Image ({src, ...rest}) {
-    src = src && src?.includes('https://')
+    // src = src && src?.includes('https://')
+    src = src 
     ? src
     : `http://localhost:3030/${src}`
     return (
